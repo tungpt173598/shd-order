bee7dc28d327d41495918300a3e03355bb191ec3
Tung.Phan <tung.phan@digitran.asia>
2024-03-06 13:28:10 +0900

    First commit

---
+++ app/Console/Commands/Test.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class Test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        dd(bcrypt('123'));
+    }
+}
